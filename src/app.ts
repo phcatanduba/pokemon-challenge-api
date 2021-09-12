4cf1422230f7adf003309d5a8ad2f46649514a3a
Pedro Henrique <phcatanduba@gmail.com>
2021-09-12 14:31:26 -0300

    setup

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,17 @@
-import './setup';
+import "./setup";
 
-import express from 'express';
-import cors from 'cors';
-import 'reflect-metadata';
+import express from "express";
+import cors from "cors";
+import "reflect-metadata";
 
-import connectDatabase from './database';
-
-import * as userController from './controllers/userController';
-import * as middleware from './middlewares/authenticate';
+import connectDatabase from "./database";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/sign-up', userController.signup);
-
-app.post('/sign-in', userController.signin);
-
-app.post(
-    '/my-pokemons/:id/add',
-    middleware.authenticate,
-    userController.addPokemon
-);
-
-app.post(
-    '/my-pokemons/:id/remove',
-    middleware.authenticate,
-    userController.removePokemon
-);
-
-app.get('/pokemons', middleware.authenticate, userController.getPokemons);
-
-app.post('/file', (req, res) => {
-    console.log(req.body);
-});
-
 export async function init() {
-    await connectDatabase();
+  await connectDatabase();
 }
 
 export default app;
